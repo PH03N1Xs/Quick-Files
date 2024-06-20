a0c3f6f12e02b4206456a0c815bb4075b3abccbb
RIQUELMEDMR <73765375+RIQUELMEDMR@users.noreply.github.com>
2024-06-20 01:01:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
-
 const releases = [
     {
         name: 'Baixar Kung Fu Panda',
         url: 'https://github.com/PHOENIXSRD/quickfiles/releases/download/v5.9-beta.3/Kung.Fu.Panda.mp4'
     },
     {
-        name: 'Versão 0.9.0',
-        url: 'https://github.com/usuario/repo/releases/download/v0.9.0/arquivo.zip'
+        name: 'Ferramenta Útil v2.1',
+        url: 'https://github.com/seu-usuario/seu-repositorio/releases/download/v2.1/ferramenta-util-v2.1.zip'
+    },
+    {
+        name: 'Plugin Poderoso v3.5',
+        url: 'https://github.com/seu-usuario/seu-repositorio/releases/download/v3.5/plugin-poderoso-v3.5.zip'
     }
+    // Adicione quantos itens de download desejar, seguindo a mesma estrutura
 ];
 
 const downloadList = document.getElementById('download-list');
